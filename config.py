e44401323564e250df358b1037983b0518175510
gaurav <gaurav4k@gmail.com>
2023-01-22 19:10:05 +0530

    vars set

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 class Config:
-    BOT_TOKEN = os.environ.get("BOT_TOKEN","")
-    HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY","")
-    AUTHORIZED_USERS = [int(user) for user in os.environ.get("AUTHORIZED_USERS").split(" ")]
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5935022610:AAEcec2VplFyjPUZeH_hncHHMauwcPy57fM")
+    HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", "e8b351fa-93ca-416c-b52c-589e38d9f6a3")
+    AUTHORIZED_USERS = [int(user) for user in os.environ.get("AUTHORIZED_USERS", "2109516065").split(" ")]
     TG_CHARACTER_LIMIT = 4000 
-    HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME","")
+    HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME","linkshortfybot")
